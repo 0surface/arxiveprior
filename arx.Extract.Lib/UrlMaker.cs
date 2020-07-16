9a474f69247d3c0eacfd44320621ee0328b8f08f
0surface <tms.0xvoid@gmail.com>
2020-07-16 14:52:35 +0100

    Added Url maker class and method, url request param dto class

---
+++ arx.Extract.Lib/UrlMaker.cs
@@ -0,0 +1,25 @@
+﻿namespace arx.Extract.Lib
+{
+    public class UrlMaker
+    {
+        public static string RequestUrlForItemsBetweenDates(UrlParams urlParams)
+        {
+            string maxResultsQuery = urlParams.ItemsPerRequest > 0 ?
+                                       $"&max_results={urlParams.ItemsPerRequest}"
+                                      : "0";
+            string baseUrl = string.IsNullOrEmpty(urlParams.QueryBaseUrl) ?
+                                string.Empty
+                                : "http://export.arxiv.org/api/query?search_query=";
+
+            string subjectQuery = urlParams.SubjectCode.Contains(".") ?
+                                $"cat:{ urlParams.SubjectCode}"
+                                : $"cat:{ urlParams.SubjectCode}*";
+
+            string start = urlParams?.QueryFromDate.ToString("yyyyMMddHHmm");
+            string end = urlParams?.QueryToDate.ToString("yyyyMMddHHmm");
+            string dateQueryString = $"+AND+submittedDate:[{start}+TO+{end}]";
+
+            return $"{baseUrl}{subjectQuery}{dateQueryString}{maxResultsQuery}";
+        }
+    }
+}
---
+++ arx.Extract.Lib/UrlParams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace arx.Extract.Lib
+{
+    public class UrlParams
+    {
+        public string SubjectCode { get; set; }
+        public DateTime QueryFromDate { get; set; }
+        public DateTime QueryToDate { get; set; }
+        public int ItemsPerRequest { get; set; }
+        public int StartIndex { get; set; }
+        public string QueryBaseUrl { get; set; }
+    }
+}
