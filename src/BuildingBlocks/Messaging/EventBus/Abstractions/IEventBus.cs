5fb96254382ed0b02475176d1cc5c391c9bd3246
0surface <tms.0xvoid@gmail.com>
2020-07-16 20:51:09 +0100

    Added EventBus project and Abstractions

---
+++ src/BuildingBlocks/Messaging/EventBus/Abstractions/IEventBus.cs
@@ -0,0 +1,13 @@
+﻿using EventBus.Events;
+
+namespace EventBus.Abstractions
+{
+    public interface IEventBus
+    {
+        void Publish(IntegrationEvent @event);
+
+        void Subscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntgeationEventHandler<T>;
+    }
+}
---
+++ src/BuildingBlocks/Messaging/EventBus/Abstractions/IIntgeationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using EventBus.Events;
+using System.Threading.Tasks;
+
+namespace EventBus.Abstractions
+{
+    public interface IIntgeationEventHandler<in TInterationEvent>  : IIntegrationEventHandler
+        where TInterationEvent : IntegrationEvent
+    {
+        Task Handle(TInterationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+    }
+}
