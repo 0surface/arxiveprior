85a4f23a3e10fced693bf1080d0f9612a4133a09
0surface <tms.0xvoid@gmail.com>
2020-07-16 14:52:05 +0100

    Added Archive fetch service class, interface, methods

---
+++ arx.Extract.Lib/ArchiveFetch.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace arx.Extract.Lib
+{
+    public interface IArchiveFetch
+    {
+        Task<ArxivItem> GetArxivItems(string url);
+    }
+    public class ArchiveFetch : IArchiveFetch
+    {
+        public async Task<ArxivItem> GetArxivItems(string url)
+        {
+            string result;
+            ArxivItem item = new ArxivItem();
+            try
+            {
+                result = await HttpQuery(url);
+
+                item = DeserializeXml(result);
+                item.EntryList = item?.EntryList?.OrderBy(x => x.PublishDate)?.ToList();
+
+                return item != null ? item : new ArxivItem();
+            }
+            catch (Exception ex)
+            {
+                item.Error = ex;
+            }
+            return item;
+        }
+
+        private ArxivItem DeserializeXml(string result)
+        {
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.LoadXml(result.ToString());
+
+            XmlNodeReader xreader = new XmlNodeReader(xdoc);
+            XmlSerializer deserializer = new XmlSerializer(typeof(ArxivItem));
+            var item = (ArxivItem)deserializer.Deserialize(xreader);
+
+            return item;
+        }
+
+        private async Task<string> HttpQuery(string url)
+        {
+            string result;
+            using (var httpClient = new HttpClient())
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                var response = await httpClient.SendAsync(request);
+                result = await response.Content.ReadAsStringAsync();
+            }
+
+            return result;
+        }
+    }
+}
