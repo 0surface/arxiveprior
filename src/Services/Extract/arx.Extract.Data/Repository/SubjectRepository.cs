3e8b4d5074a50cb88d9a31bee51af471495f848c
0surface <tms.0xvoid@gmail.com>
2020-07-18 12:06:06 +0100

    Add repo class

---
+++ src/Services/Extract/arx.Extract.Data/Repository/SubjectRepository.cs
@@ -0,0 +1,33 @@
+﻿using arx.Extract.Data.Entities;
+using Microsoft.Azure.Cosmos.Table;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace arx.Extract.Data.Repository
+{
+    public class SubjectRepository
+    {
+        private CloudTable subjectTable = null;
+        public SubjectRepository(string connectionString, string table)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            subjectTable = tableClient.GetTableReference("Subjects");
+            subjectTable.CreateIfNotExists();
+        }
+
+        public async Task<IEnumerable<SubjectEntity>> All()
+        {
+            var query = new TableQuery<SubjectEntity>();
+
+            var entities = subjectTable.ExecuteQuery(query);
+
+            return await Task.FromResult(entities);
+        }
+
+        public async void Seed()
+        {
+            await Task.Run(() => { });
+        }
+    }
+}
