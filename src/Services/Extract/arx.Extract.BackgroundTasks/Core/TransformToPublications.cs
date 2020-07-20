28ab47d44844a4bebaaa5d0705909df017916d85
0surface <tms.0xvoid@gmail.com>
2020-07-20 17:50:25 +0100

    Add class to map publication types, stub method

---
+++ src/Services/Extract/arx.Extract.BackgroundTasks/Core/TransformToPublications.cs
@@ -0,0 +1,28 @@
+﻿using arx.Extract.Data.Entities;
+using arx.Extract.Lib;
+using System;
+using System.Collections.Generic;
+
+namespace arx.Extract.BackgroundTasks.Core
+{
+    public static class TransformToPublications
+    {
+        public static List<PublicationItemEntity> TransformArxivEntriesToPublications(List<Entry> allArxivEntries)
+        {
+            List<PublicationItemEntity> results = new List<PublicationItemEntity>();
+            try
+            {
+                foreach (var item in allArxivEntries)
+                {
+
+                }
+            }
+            catch (Exception)
+            {
+                //TODo:Error Handling
+            }
+
+            return results;
+        }
+    }
+}
