df689221c6ef4f147779ada4eaf8e98428483183
0surface <tms.0xvoid@gmail.com>
2020-07-22 20:54:33 +0100

    Created arxiv atom api to own types tranformer method

--- src/Services/Extract/arx.Extract.BackgroundTasks/Core/TransformToPublications.cs
+++ src/Services/Extract/arx.Extract.BackgroundTasks/Core/TransformToPublications.cs
@@ -1,28 +1,83 @@
 ﻿using arx.Extract.Data.Entities;
 using arx.Extract.Lib;
+using arx.Extract.Types;
+using AutoMapper;
+using Microsoft.OData.UriParser;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace arx.Extract.BackgroundTasks.Core
 {
-    public static class TransformToPublications
+    public interface ITransformService
     {
-        public static List<PublicationItemEntity> TransformArxivEntriesToPublications(List<Entry> allArxivEntries)
+        List<PublicationItem> TransformArxivEntriesToPublications(List<Entry> allArxivEntries);
+    }
+
+    public class TransformService : ITransformService
+    {
+        private readonly IMapper _mapper;
+
+        public TransformService(IMapper mapper)
         {
-            List<PublicationItemEntity> results = new List<PublicationItemEntity>();
+            _mapper = mapper;
+        }
+
+        public  List<PublicationItem> TransformArxivEntriesToPublications(List<Entry> allArxivEntries)
+        {
+            List<PublicationItem> results = new List<PublicationItem>();
+
+            foreach (var entry in allArxivEntries)
+            {
+                var publication = MapEntryToPublication(entry);
+                if(publication != null)
+                {
+                    results.Add(publication);
+                }
+            }
+          
+            return results;
+        }
+
+        private PublicationItem MapEntryToPublication(Entry item)
+        {
+            PublicationItem pub = new PublicationItem();
             try
             {
-                foreach (var item in allArxivEntries)
-                {
+                pub.ArxivId = item.Id.GetSubStringAfterCharValue('/');
+                pub.VersionTag = pub.ArxivId.GetSubStringAfterCharValue('v', true);
+                pub.CanonicalArxivId = pub.ArxivId.Replace(pub.VersionTag, "");
+                pub.PublishedDate = item.PublishDate;
+                pub.UpdatedDate = item.UpdatedDate;
+                pub.Title = item.Title;
+                pub.Abstract = item.Summary;
+                pub.Comment = item.comment;
+                pub.JournalReference = item.journal_ref;
+                pub.Doi = item.doi;
 
-                }
+                pub.PrimarySubjectCode = item.primary_category.Subject.Trim();
+
+                item.Subjects?.Where(s => s.Subject != pub.PrimarySubjectCode)
+                            ?.ToList()
+                            ?.ForEach(s => pub.SubjectCodes.Add(s.Subject));
+
+                pub.PdfLink = item?.Links
+                                    ?.Where(l => l.Title == "pdf")
+                                    ?.Select(l => l.Href)
+                                    ?.SingleOrDefault() ?? string.Empty;
+                pub.DoiLink = item?.Links
+                                    ?.Where(l => l.Title == "doi")
+                                    ?.Select(l => l.Href)
+                                    ?.SingleOrDefault() ?? string.Empty;
+
+                pub.Authors = _mapper.Map<List<AuthorItem>>(item.Authors);
             }
             catch (Exception)
             {
-                //TODo:Error Handling
+                return null;
             }
-
-            return results;
+            return pub;
         }
     }
 }
