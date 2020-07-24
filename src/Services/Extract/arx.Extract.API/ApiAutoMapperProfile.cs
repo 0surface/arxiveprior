8824c59360198460cab91de8d49776deb411fdcd
0surface <tms.0xvoid@gmail.com>
2020-07-24 12:03:11 +0100

    Set up config values, automapper profile

---
+++ src/Services/Extract/arx.Extract.API/ApiAutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using arx.Extract.Data.Entities;
+using arx.Extract.Types;
+using AutoMapper;
+
+namespace arx.Extract.API
+{
+    public class ApiAutoMapperProfile : Profile
+    {
+        public ApiAutoMapperProfile()
+        {
+            CreateMap<PublicationItemEntity, PublicationItem>();
+        }
+    }
+}
