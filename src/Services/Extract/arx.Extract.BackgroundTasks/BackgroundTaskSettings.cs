396429e2d7a9c048e6e021dcab341821d67a6a35
0surface <tms.0xvoid@gmail.com>
2020-07-27 17:12:55 +0100

    Add ArchiveTerminateDate to Config files, class

--- src/Services/Extract/arx.Extract.BackgroundTasks/BackgroundTaskSettings.cs
+++ src/Services/Extract/arx.Extract.BackgroundTasks/BackgroundTaskSettings.cs
@@ -1,4 +1,6 @@
-﻿namespace arx.Extract.BackgroundTasks
+﻿using System;
+
+namespace arx.Extract.BackgroundTasks
 {
     public class BackgroundTaskSettings
     {
@@ -17,6 +19,7 @@
 
         public string ExtractionMode { get; set; }
         public bool ArchiveModeIsActive { get; set; }
+        public DateTime ArchiveTerminateDate { get; set; }
         public string ArchiveJobName { get; set; }
         public string SeedJobName { get; set; }
         public string JournalJobName { get; set; }
