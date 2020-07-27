02303e9785a12e5883d694fd9cb1d0b8b557a493
0surface <tms.0xvoid@gmail.com>
2020-07-27 14:27:55 +0100

    API - modified app settings files, add config settings class, adjusted host environments

---
+++ src/Services/Extract/arx.Extract.API/ExtractApiConfigurationSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace arx.Extract.API
+{
+    public class ExtractApiConfigurationSettings
+    {
+        public string SubscriptionClientName { get; set; }
+        public string StorageConnectionString { get; set; }
+
+        public string SubjectTableName { get; set; }
+        public string PublicationTableName { get; set; }
+        public string JobTableName { get; set; }
+        public string JobItemTableName { get; set; }
+        public string FulfillmentTableName { get; set; }
+        public string FulfillmentItemTableName { get; set; }
+
+        public string ExtractionMode { get; set; }
+        public string ArchiveJobName { get; set; }
+        public string SeedJobName { get; set; }
+        public string JournalJobName { get; set; }
+    }
+}
