7a643f1eed09b5afeca90cb01d5bddd6fcb6c700
0surface <tms.0xvoid@gmail.com>
2020-07-28 02:18:54 +0100

    Add Collection Extension method to add item in list if not null

---
+++ src/BuildingBlocks/Extensions/Common.Util/Collections/CollectionsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace System.Collections.Generic
+{
+    public static class CollectionsExtensions
+    {
+        /// <summary>
+        ///  Adds an object to the end of the System.Collections.Generic.List if the provided value 
+        ///  is not null and the List is not null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="value"></param>
+        public static void AddIfNotNull<T>(this IEnumerable<T> list, T value)
+        {
+            if (list != null && value != null)
+            {
+                list.AddIfNotNull(value);
+            }
+        }
+    }
+}
