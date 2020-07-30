fa83d3a769b5f8a94c81939860dfc3472cb0303c
0surface <tms.0xvoid@gmail.com>
2020-07-30 10:13:00 +0100

    Set up Journal API, Tasks, Domain and Infrastructure projects

---
+++ src/Services/Journal/Journal.Infrastructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Journal.Infrastructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
