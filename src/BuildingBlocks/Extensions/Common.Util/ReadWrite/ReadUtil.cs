6b13995f33a522c34b9ce09b05b22e7c77f18046
0surface <tms.0xvoid@gmail.com>
2020-07-30 12:51:46 +0100

    Add method to read embedded resource files

---
+++ src/BuildingBlocks/Extensions/Common.Util/ReadWrite/ReadUtil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Common.Util.ReadWrite
+{
+    public class ReadUtil
+    {
+        public static string ReadDocumentFromExecutingAssembly(string resourceName)
+        {
+            string data = "";
+            try
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    data = reader.ReadToEnd();
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            return data;
+        }
+    }
+}
