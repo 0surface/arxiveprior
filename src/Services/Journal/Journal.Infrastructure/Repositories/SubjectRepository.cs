0097b5bee26c3273d65328ec7a1ae5b6c4730dfd
0surface <tms.0xvoid@gmail.com>
2020-08-02 16:39:22 +0100

    Journal.Infrastructure - add repository, adjust launchsettings.json

---
+++ src/Services/Journal/Journal.Infrastructure/Repositories/SubjectRepository.cs
@@ -0,0 +1,33 @@
+﻿using Journal.Domain.AggregatesModel.SubjectAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Journal.Infrastructure.Repositories
+{
+    public interface ISubjectRepository
+    {
+        List<Subject> GetAllSubjects();
+    }
+    public class SubjectRepository : ISubjectRepository
+    {
+        private readonly JournalContext _context;
+
+        public SubjectRepository(JournalContext context)
+        {
+            _context = context;
+        }
+
+        public List<Subject> GetAllSubjects()
+        {
+            try
+            {
+                return _context.Subjects.ToList();
+            }
+            catch (Exception)
+            {
+                return new List<Subject>();
+            }
+        }
+    }
+}
