3cbc1dc4e481105432e69e6cf698d82daa043239
0surface <tms.0xvoid@gmail.com>
2020-08-03 16:13:46 +0100

    Add Fulfillment domain, DbContext, DbContext Factory and apply db migration

---
+++ src/Services/Journal/Journal.Domain/AggregatesModel/FulfillmentAggregate/Fulfillment.cs
@@ -0,0 +1,22 @@
+﻿using Journal.Domain.SeedWork;
+using System;
+
+namespace Journal.Domain.AggregatesModel.JobAggregate
+{
+    public class Fulfillment
+        : Entity, IAggregateRoot
+    {
+        public string EventId { get; set; }
+        public string ExtractionFulfillmentId { get; set; }
+        public ProcessTypeEnum JournalType { get; set; }
+        public int ArticlesCount { get; set; }
+
+        public int InsertedCount { get; set; }
+        public int UpdatedCount { get; set; }
+        public int TotalProcessedCount { get; set; }
+        public int ProcessingTimeInMilliseconds { get; set; }
+
+        public DateTime JobStartedDate { get; set; }
+        public DateTime JobCompletedDate { get; set; }
+    }
+}
---
+++ src/Services/Journal/Journal.Infrastructure/FulfillmentContext.cs
@@ -0,0 +1,17 @@
+﻿using Journal.Domain.AggregatesModel.JobAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journal.Infrastructure
+{    
+    public class FulfillmentContext : DbContext
+    {
+        public DbSet<Fulfillment> Fulfillments { get; set; }
+
+        public FulfillmentContext(DbContextOptions<FulfillmentContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
