09711715e0c771dd9ebcaf5c7b4ea2ced79854ec
0surface <tms.0xvoid@gmail.com>
2020-08-12 17:16:54 +0100

    Added lazy loading nuget pacakge, configured in start up

--- src/Services/Journal/Journal.Infrastructure/Startup.cs
+++ src/Services/Journal/Journal.Infrastructure/Startup.cs
@@ -73,6 +73,7 @@ namespace Journal.Infrastructure
                         {
                             sqlOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
                         });
+                    options.UseLazyLoadingProxies();
                 },
                 ServiceLifetime.Scoped //Showing explicitly that the DbContext is shared across the HTTP request scope (graph of objects started in the HTTP request)
                 );
