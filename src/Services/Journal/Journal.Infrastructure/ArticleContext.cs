bb7c34d8455b1e8951df7840e46c1b26cc78b4f9
0surface <tms.0xvoid@gmail.com>
2020-08-12 15:52:26 +0100

    Add Article Context class

---
+++ src/Services/Journal/Journal.Infrastructure/ArticleContext.cs
@@ -0,0 +1,28 @@
+﻿using Journal.Domain.AggregatesModel.ArticleAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journal.Infrastructure
+{
+    public class ArticleContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+
+        public ArticleContext(DbContextOptions<ArticleContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Article>(x =>
+            {
+                x.ToTable("Article");
+                x.HasMany(p => p.PaperVersions).WithOne(x => x.Article)
+                .Metadata.PrincipalToDependent.SetPropertyAccessMode(PropertyAccessMode.Field);
+            });
+
+            modelBuilder.Entity<PaperVersion>(x => 
+            {
+                x.ToTable("Version").HasKey(k => k.Id);
+                x.HasOne(p => p.Article).WithMany(p => p.PaperVersions);
+            });
+        }
+    }
+}
