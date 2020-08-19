58a30d343040258905305175f39cedb2c250fca7
0surface <tms.0xvoid@gmail.com>
2020-08-19 16:52:40 +0100

    Added protos as grpc service. Created  journal service, automapper profiles

--- src/Services/Extract/arx.Extract.API/ApiAutoMapperProfile.cs
+++ src/Services/Extract/arx.Extract.API/ApiAutoMapperProfile.cs
@@ -1,6 +1,11 @@
-﻿using arx.Extract.Data.Entities;
+﻿using arx.Extract.API.Services;
+using arx.Extract.Data.Entities;
 using arx.Extract.Types;
 using AutoMapper;
+using Google.Protobuf;
+using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
+using System;
 using System.Collections.Generic;
 
 namespace arx.Extract.API
@@ -14,7 +19,36 @@ namespace arx.Extract.API
 
             CreateMap<FulfillmentEntity, Fulfillment>();
             CreateMap<FulfillmentItemEntity, FulfillmentItem>();
+            CreateMap<PublicationItemEntity, Publication>()
+                .ForMember(m => m.PublishedDate, opt => opt.MapFrom(entity => Timestamp.FromDateTime(entity.PublishedDate)))
+                .ForMember(m => m.UpdatedDate, opt => opt.MapFrom(entity => Timestamp.FromDateTime(entity.UpdatedDate)))
+                .ForMember(m => m.SubjectCodes, opt => opt.MapFrom<GrpcSubjectCodesResolver>())
+                .ForMember(m => m.Authors, opt => opt.MapFrom<GrpcAuthorListAggregateResolver>());
         }
+
+        public class GrpcSubjectCodesResolver : IValueResolver<PublicationItemEntity, Publication, Google.Protobuf.Collections.RepeatedField<string>>
+        {
+            public RepeatedField<string> Resolve(PublicationItemEntity source, Publication destination, RepeatedField<string> destMember, ResolutionContext context)
+            {
+                destination.SubjectCodes.AddRange(source.SubjectCodes);
+                return destination.SubjectCodes;
+            }
+        }
+
+        public class GrpcAuthorListAggregateResolver : IValueResolver<PublicationItemEntity, Publication, Google.Protobuf.Collections.RepeatedField<string>>
+        {
+            public RepeatedField<string> Resolve(PublicationItemEntity source, Publication destination, RepeatedField<string> destMember, ResolutionContext context)
+            {
+                if (source?.Authors != null)
+                    destination.Authors.AddRange(source.Authors);
+
+                if (source?.AuthorSpillOverList != null)
+                    destination.Authors.AddRange(source.AuthorSpillOverList);
+
+                return destination.Authors;
+            }
+        }
+
 
         public class AuthorListAggregateResolver : IValueResolver<PublicationItemEntity, PublicationItem, List<string>>
         {
@@ -24,9 +58,7 @@ namespace arx.Extract.API
                     destination.Authors.AddRange(source.Authors);
 
                 if (source?.AuthorSpillOverList != null && source.AuthorSpillOverList.Count > 0)
-                    destination.Authors.AddRange(source?.AuthorSpillOverList);
-
-               
+                    destination.Authors.AddRange(source.AuthorSpillOverList);
 
                 return destination.Authors;
             }
---
+++ src/Services/Extract/arx.Extract.API/Services/JournalService.cs
@@ -0,0 +1,81 @@
+﻿using arx.Extract.Data.Repository;
+using AutoMapper;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace arx.Extract.API.Services
+{
+    public class JournalService : ExtractService.ExtractServiceBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IPublicationRepository _publicationRepo;
+        private readonly IFulfillmentRepository _fulfillmentRepo;
+
+        public JournalService(IMapper mapper,
+            IPublicationRepository publicationRepo,
+            IFulfillmentRepository fulfillmentRepo)
+        {
+            _mapper = mapper;
+            _publicationRepo = publicationRepo;
+            _fulfillmentRepo = fulfillmentRepo;
+        }
+        public override Task<PublicationResponse> GetExtractedPublications(PublicationRequest request, ServerCallContext context)
+        {
+            PublicationResponse response = new PublicationResponse() { Status = ResponseStatus.Failure };
+
+            //Validate Input DateTime value
+            DateTime queryFromDate = request.FromDate != null ? request.FromDate.ToDateTime() : DateTime.MinValue;
+            if (queryFromDate == DateTime.MinValue)
+            {
+                response.Status = ResponseStatus.Invalid;
+                return Task.FromResult(response);
+            }
+
+            if (request.ProcessedDataType == ProcessedDataType.Archive)
+                return GetArchivePublications(response, queryFromDate);
+
+
+            return Task.FromResult(response);
+        }
+
+        private async Task<PublicationResponse> GetArchivePublications(PublicationResponse response, DateTime queryFromDate)
+        {
+            try
+            {
+                var fulfillment = _fulfillmentRepo.GetLastSuccessfulArchiveFulfillment(queryFromDate).Result;
+
+                if (fulfillment == null)
+                {
+                    string msg = $"Unable to find Archive Fulfillment record after queryFromDate [{queryFromDate}]";
+                    response.ErrorMessage = msg;
+                    Log.Fatal(msg);
+                    return await Task.FromResult(response);
+                }
+                else
+                {
+                    var entities = await _publicationRepo.GetByFulfilmentId(fulfillment.FulfillmentId.ToString());
+
+                    response.Publications.AddRange(_mapper.Map<List<Publication>>(entities));
+                    response.Status = ResponseStatus.Success;
+                    response.FulfillmentId = fulfillment.FulfillmentId.ToString();
+                    response.QueryFromDate = Timestamp.FromDateTime(fulfillment.QueryFromDate);
+                    response.QueryToDate = Timestamp.FromDateTime(fulfillment.QueryToDate);
+
+                    return response;
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = $"Attmepting to retrieve Archive Publications data.[{ ex.Message}]";
+                Log.Fatal(ex, msg);
+                response.ErrorMessage = msg;
+                return response;
+            }
+        }
+    }
+}
+
