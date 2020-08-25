6397dd176e7578b32da3da31665888e0c7ef84db
0surface <tms.0xvoid@gmail.com>
2020-08-25 10:22:29 +0100

    Add grpc caller util method

---
+++ src/Services/Journal/Journal.BackgroundTasks/Services/GrpcCallerService.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace Journal.BackgroundTasks.Services
+{
+    public static class GrpcCallerService
+    {
+        public static async Task<TResponse> CallService<TResponse>(string urlGrpc, Func<GrpcChannel, Task<TResponse>> func)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2Support", true);
+
+            var channel = GrpcChannel.ForAddress(urlGrpc);
+
+            /*
+            using var httpClientHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; }
+            };
+
+            */
+
+            Log.Information("Creating grpc client base address urlGrpc ={@urlGrpc}, BaseAddress={@BaseAddress} ", urlGrpc, channel.Target);
+
+            try
+            {
+                return await func(channel);
+            }
+            catch (RpcException e)
+            {
+                Log.Error("Error calling via grpc: {Status} - {Message}", e.Status, e.Message);
+                return default;
+            }
+            finally
+            {
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", false);
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2Support", false);
+            }
+        }
+    }
+}
