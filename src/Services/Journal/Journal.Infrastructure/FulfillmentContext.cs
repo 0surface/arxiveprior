0bd02dc2698b726a0e92b056d12e41b13b65a7e7
0surface <tms.0xvoid@gmail.com>
2020-08-25 10:28:48 +0100

    Relocate design time db factories to own dbContext class files

--- src/Services/Journal/Journal.Infrastructure/FulfillmentContext.cs
+++ src/Services/Journal/Journal.Infrastructure/FulfillmentContext.cs
@@ -1,8 +1,12 @@
 ﻿using Journal.Domain.AggregatesModel.JobAggregate;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace Journal.Infrastructure
-{    
+{
     public class FulfillmentContext : DbContext
     {
         public DbSet<Fulfillment> Fulfillments { get; set; }
@@ -12,6 +16,26 @@ namespace Journal.Infrastructure
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
+        }
+    }
+
+    public class FulfillmentDbContextFactory : IDesignTimeDbContextFactory<FulfillmentContext>
+    {
+        public static readonly string EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        public FulfillmentContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory()))
+                            .AddJsonFile($"appsettings.{EnvironmentName}.json", optional: false, reloadOnChange: true)
+                            .AddEnvironmentVariables()
+                            .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<FulfillmentContext>();
+
+            optionsBuilder.UseSqlServer(config["ConnectionString"], sqlServerOptionsAction: o => o.MigrationsAssembly("Journal.Infrastructure"));
+
+            return new FulfillmentContext(optionsBuilder.Options);
         }
     }
 }
--- src/Services/Journal/Journal.Infrastructure/SubjectContext.cs
+++ src/Services/Journal/Journal.Infrastructure/SubjectContext.cs
@@ -1,5 +1,9 @@
 ﻿using Journal.Domain.AggregatesModel.SubjectAggregate;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace Journal.Infrastructure
 {
@@ -14,6 +18,26 @@ namespace Journal.Infrastructure
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
+        }
+    }
+
+    public class SubjectDbContextFactory : IDesignTimeDbContextFactory<SubjectContext>
+    {
+        public static readonly string EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        public SubjectContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory()))
+                            .AddJsonFile($"appsettings.{EnvironmentName}.json", optional: false, reloadOnChange: true)
+                            .AddEnvironmentVariables()
+                            .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<SubjectContext>();
+
+            optionsBuilder.UseSqlServer(config["ConnectionString"], sqlServerOptionsAction: o => o.MigrationsAssembly("Journal.Infrastructure"));
+
+            return new SubjectContext(optionsBuilder.Options);
         }
     }
 }
