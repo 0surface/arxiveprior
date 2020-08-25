ac2fe5b774ae38b03052b55e20f2438a0eb042ac
0surface <tms.0xvoid@gmail.com>
2020-08-25 10:21:28 +0100

    Rename/relocate namespace of service

---
+++ src/Services/Journal/Journal.BackgroundTasks/Services/ITransformService.cs
@@ -0,0 +1,11 @@
+﻿using Journal.BackgroundTasks.Types;
+using Journal.Domain.AggregatesModel.ArticleAggregate;
+using System.Collections.Generic;
+
+namespace Journal.BackgroundTasks.Services
+{
+    public interface ITransformService
+    {
+        (bool, List<Article>) MapToDomain(List<PublicationItemDto> publications, int journalProcessId);
+    }
+}
---
+++ src/Services/Journal/Journal.BackgroundTasks/Services/TransformService.cs
@@ -0,0 +1,65 @@
+﻿using Journal.BackgroundTasks.Types;
+using Journal.Domain.AggregatesModel.ArticleAggregate;
+using Serilog;
+using System;
+using System.Collections.Generic;
+
+namespace Journal.BackgroundTasks.Services
+{
+    public class TransformService : ITransformService
+    {
+        public TransformService()
+        {
+
+        }
+
+        public (bool, List<Article>) MapToDomain(List<PublicationItemDto> publications, int journalProcessId)
+        {
+            List<Article> articles = new List<Article>();
+            try
+            {
+                publications?.ForEach(dto =>
+                {
+                    articles.AddIfNotNull(MapArxivPublicationToArticle(dto, journalProcessId));
+                });
+
+                return (articles.Count == publications.Count, articles);
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, $"TransformService.MapToDomain has thrown an exception");
+            }
+
+            return (articles.Count == publications.Count, articles);
+        }
+
+        private Article MapArxivPublicationToArticle(PublicationItemDto dto, int jpId)
+        {
+            try
+            {
+                Article article = new Article(jpId,
+                    dto.ArxivId,
+                    dto.PublishedDate,
+                    dto.UpdatedDate,
+                    dto.Title,
+                    dto.Abstract,
+                    dto.Comment,
+                    dto.PrimarySubjectCode,
+                    dto.JournalReference,
+                    dto.MscCodes,
+                    dto.AcmCodes,
+                    dto.Doi,
+                    dto.DoiLinks,
+                    dto.SubjectCodes,
+                    dto.Authors);
+
+                return article;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, $"Arxiv Id [{dto.ArxivId}] - MapArxivPublicationToArticle has thrown an exception");
+                return null;
+            }
+        }
+    }
+}
