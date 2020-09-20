2b87888a167da9dc910cbeb2992fefd2d3a66203
0surface <tms.0xvoid@gmail.com>
2020-09-20 11:24:10 +0100

    Journal API- implement gRPC stream endpoint

--- src/Services/Journal/Journal.API/Controllers/ArticlesController.cs
+++ src/Services/Journal/Journal.API/Controllers/ArticlesController.cs
@@ -1,5 +1,7 @@
 ﻿using Journal.API.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace Journal.API.Controllers
 {
@@ -7,9 +9,33 @@ namespace Journal.API.Controllers
     [ApiController]
     public class ArticlesController : Controller
     {
-        public ArticlesController(IArticlesService articlesService)
-        {
+        private readonly IArticleService _articlesService;
 
+        public ArticlesController(IArticleService articlesService)
+        {
+            _articlesService = articlesService;
         }
+
+        [HttpGet()]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetArticles(string fulfillmentId)
+        {
+            if (string.IsNullOrEmpty(fulfillmentId))
+                return StatusCode((int)HttpStatusCode.BadRequest);
+
+            var result = await _articlesService.GetArticles(fulfillmentId);
+
+            if (result == null || result.PublicationItems == null)
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+
+            if (result.PublicationItems.Count == 0)
+                return NoContent();
+
+            return Json(result);
+        }
+
     }
 }
